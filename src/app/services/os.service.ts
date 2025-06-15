93480312992e30483828d1cb87b24673c48fc13c
gabrielfernandesgf <djgabrielfernandes01@gmail.com>
2025-06-15 19:20:30 -0300

    criação agora dos acessorios esta dentro do cadastro do veiculo, desabilitado a tela de cadastro de acessorio e implementado junto ao cadastro do veiculo.

--- src/app/services/os.service.ts
+++ src/app/services/os.service.ts
@@ -20,6 +20,14 @@ export class OsService {
     return this.http.get<OS>(`${this.apiUrl}/${id}`);
   }
 
+  findByPlaca(placa: string): Observable<OS[]> {
+    return this.http.get<OS[]>(`${this.apiUrl}/placa/${placa}`);
+  }
+
+  findByStatus(status: string): Observable<OS[]> {
+    return this.http.get<OS[]>(`${this.apiUrl}/status/${status}`);
+  }
+
   save(osDTO: OSDTO): Observable<OS> {
     return this.http.post<OS>(this.apiUrl, osDTO);
   }
